4ac553d12a66199f12f8bda91d05eaac3a88caeb
Kauã Werle <kauawerle19@gmail.com>
2022-03-15 16:56:55 -0300

    Change database

---
+++ Migration_Estudo1/Migrations/20220315185449_Empty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Migration_Estudo1.Migrations
+{
+    public partial class Empty : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+
+            mb.Sql("INSERT INTO Cliente(Nome,Idade) VALUES('Maria', 22)");
+            mb.Sql("INSERT INTO Cliente(Nome,Idade) VALUES('Pedro', 22)");
+
+            mb.Sql("INSERT INTO Pedidos(Item,Quantidade,Preco,DataPedido,ClienteId) VALUES('Caderno',2,7.99,'2020-01-01',(SELECT CienteId FROM Cliente Where Nome='Maria'))");
+            mb.Sql("INSERT INTO Pedidos(Item,Quantidade,Preco,DataPedido,ClienteId) VALUES('Borracha',6,2.99,'2020-01-01,(SELECT CienteId FROM Cliente Where Nome='Maria'))");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Cliente");
+            mb.Sql("DELETE FROM Pedido");
+        }
+    }
+}
