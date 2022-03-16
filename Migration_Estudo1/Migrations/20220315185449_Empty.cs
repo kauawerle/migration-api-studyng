d205fc01e5282e105815ba2dc0b33a6fd404fbca
Kauã Werle <kauawerle19@gmail.com>
2022-03-16 10:19:43 -0300

    Change migration

--- Migration_Estudo1/Migrations/20220315185449_Empty.cs
+++ Migration_Estudo1/Migrations/20220315185449_Empty.cs
@@ -10,8 +10,8 @@ namespace Migration_Estudo1.Migrations
             mb.Sql("INSERT INTO Cliente(Nome,Idade) VALUES('Maria', 22)");
             mb.Sql("INSERT INTO Cliente(Nome,Idade) VALUES('Pedro', 22)");
 
-            mb.Sql("INSERT INTO Pedidos(Item,Quantidade,Preco,DataPedido,ClienteId) VALUES('Caderno',2,7.99,'2020-01-01',(SELECT CienteId FROM Cliente Where Nome='Maria'))");
-            mb.Sql("INSERT INTO Pedidos(Item,Quantidade,Preco,DataPedido,ClienteId) VALUES('Borracha',6,2.99,'2020-01-01,(SELECT CienteId FROM Cliente Where Nome='Maria'))");
+            mb.Sql("INSERT INTO Pedidos(Item,Quantidade,Preco,DataPedido,ClienteId) VALUES('Caderno',2,7.99,'2020-01-01',(SELECT ClienteId FROM Cliente Where Nome='Maria'))");
+            mb.Sql("INSERT INTO Pedidos(Item,Quantidade,Preco,DataPedido,ClienteId) VALUES('Borracha',6,2.99,'2020-01-01',(SELECT ClienteId FROM Cliente Where Nome='Pedro'))");
         }
 
         protected override void Down(MigrationBuilder mb)
